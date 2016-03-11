530e85e4545e9e46f5d47d9b0bdf97517fd4490c
milworm <ruslan.prytula@gmail.com>
2016-03-11 02:22:30 -0500

    init

---
+++ tests/unit/helpers/grid-width-helper-test.js
@@ -0,0 +1,10 @@
+import { gridWidthHelper } from 'gallery/helpers/grid-width-helper';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | grid width helper');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = gridWidthHelper([42]);
+  assert.ok(result);
+});
